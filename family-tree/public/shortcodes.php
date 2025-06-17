c54777c5de2243b99fa61ad3f4e914a4a6313cea
janfichter <fichter@bk.ru>
2025-06-17 12:57:39 +0800

    Delete family-tree directory

--- family-tree/public/shortcodes.php
+++
@@ -1,7 +0,0 @@
-<?php
-function family_tree_shortcode() {
-    ob_start();
-    include plugin_dir_path(__FILE__) . '../templates/tree-template.php';
-    return ob_get_clean();
-}
-add_shortcode('family_tree', 'family_tree_shortcode');
